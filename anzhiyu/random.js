4d3d281961e10813053ab0d31e73e0baf274d4cd
KrisYung18 <KrisYung18@gmail.com>
2024-07-20 18:49:51 +0800

    Site updated: 2024-07-20 18:49:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/20/hello-world/"];function toRandomPost(){
+var posts=["2024/07/20/posttest/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
